3e9cc903e4c82a2a54236d3ece574fcea84a4d8a
Sp-X <76179646+Sp-X@users.noreply.github.com>
2022-01-16 22:37:45 +0530

    Initial commit.

---
+++ args_in_exceptions.py
@@ -0,0 +1,42 @@
+﻿#Example 1
+
+class NewValueError(ValueError):
+    def __init__(self, name, color, state):
+        self.data = (name, color, state)
+
+try:
+    raise NewValueError("Enemy warning", "Red alert", "High readiness")
+except NewValueError as nve:
+    for arg in nve.args:
+        print(arg, end='! ')
+
+
+#Example 2
+
+def print_args(args):
+    lng = len(args)
+    if lng == 0:
+        print("")
+    elif lng == 1:
+        print(args[0])
+    else:
+        print(str(args))
+
+
+try:
+    raise Exception
+except Exception as e:
+    print(e, e.__str__(), sep=' : ' ,end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
+
+try:
+    raise Exception("my", "exception")
+except Exception as e:
+    print(e, e.__str__(), sep=' : ', end=' : ')
+    print_args(e.args)
---
+++ inheritance.py
@@ -0,0 +1,28 @@
+﻿class Dog:
+    kennel = 0
+    def __init__(self, breed):
+        self.breed = breed
+        Dog.kennel += 1
+    def __str__(self):
+        return self.breed + " says: Woof!"
+
+
+class SheepDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Don't run away, Little Lamb!"
+
+
+class GuardDog(Dog):
+    def __str__(self):
+        return super().__str__() + " Stay where you are, Mister Intruder!"
+
+
+class LowlandDog(SheepDog):
+    def __str__(self):
+        return "Woof! I don't like mountains."
+
+rocky = SheepDog("Collie")
+luna = GuardDog("Dobermann")
+tommy = LowlandDog("Pug")
+
+print(tommy)
